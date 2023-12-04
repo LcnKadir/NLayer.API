c77aeed2648a6240a9fc5b0a449b87cecf6ade5b
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2023-12-04 23:11:33 +0300

    NLayer.API
    N Katmanlı mimari yapısını temel alarak Asp.Net Core ile birlikte bir API projesi gerçekleştirdim. // Based on the N Tier architectural structure Asp.Net I have implemented an API project together with Core.

---
+++ NLayer.Service/Services/ProductServiceWithNoCaching.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Model;
+using NLayer.Core.Repositories;
+using NLayer.Core.Repository;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class ProductServiceWithNoCaching : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductServiceWithNoCaching(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        {
+                _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWtihCategoryDto>>> GetProductWithCategory()
+        {
+            var product = await _productRepository.GetProductWithCategory();
+
+            var productsDto = _mapper.Map<List<ProductWtihCategoryDto>>(product);
+            return CustomResponseDto<List<ProductWtihCategoryDto>>.Success(200, productsDto);
+
+
+        }
+    }
+}
